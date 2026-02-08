8ec20e96bc2b1314537059be28a10759587fbf91
Avni Chakraverty <chakravertyavni@gmail.com>
2026-02-08 14:05:30 +0530

    Fix user model filename to lowercase

--- gym-backend/models/User.js
+++ gym-backend/models/User.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
@@ -9,12 +9,7 @@ const UserSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true
-    },
-    services: {
-        personalTrainer: { type: Boolean, default: false },
-        nutritionist: { type: Boolean, default: false },
-        weekendSession: { type: Boolean, default: false }
     }
 });
 
-module.exports = mongoose.model("User", UserSchema);
+module.exports = mongoose.model("User", userSchema);
