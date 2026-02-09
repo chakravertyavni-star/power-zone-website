864a35ad0ff8269816b19ba8ad446a09b5b9137e
Avni Chakraverty <chakravertyavni@gmail.com>
2026-02-09 19:40:14 +0530

    fix duplicate authMiddleware

--- gym-backend/routes/auth.js
+++ gym-backend/routes/auth.js
@@ -70,22 +70,6 @@ router.post("/login", async (req, res) => {
 });
 
 
-// ================= MIDDLEWARE (TOKEN CHECK) =================
-function authMiddleware(req, res, next) {
-    const authHeader = req.headers.authorization;
-
-    if (!authHeader) return res.status(401).json({ message: "No token" });
-
-    const token = authHeader.split(" ")[1];
-
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decoded;
-        next();
-    } catch {
-        res.status(401).json({ message: "Invalid token" });
-    }
-}
 
 
 // ================= SAVE SERVICES =================
